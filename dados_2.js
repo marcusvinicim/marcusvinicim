b6a2b8e6b1c99316f7d0ab4d9209f8b83aaab811
M. Vinicius Soares <85581839+marcusvinicim@users.noreply.github.com>
2021-07-29 19:42:06 -0400

    Add files via upload

---
+++ dados_2.js
@@ -0,0 +1,13 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+let precoComDesconto = preco * (1 - desconto);
+console.log(preco * (1 - desconto));
+
+let no = "Caderno ";  // String (Texto) -> sequencia de simbolos
+let categoria = "Papelaria";
+console.log( no + categoria);
+console.log("Produto: " + no
+ + ", Categoria: " + categoria
+ + ", Preço: " + preco 
+ + ", Desconto: " + desconto );
